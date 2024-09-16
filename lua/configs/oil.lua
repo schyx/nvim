2d387aa38e0164aed5bea66a9dc9d597cd46c927
Sean Cheng <seanc2841@gmail.com>
2024-09-15 23:05:26 -0400

    Add directory in oil

--- lua/configs/oil.lua
+++ lua/configs/oil.lua
@@ -19,6 +19,17 @@ local git_ignored = setmetatable({}, {
   end,
 })
 
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 local oil = require("oil")
 
 oil.setup({
@@ -47,6 +58,9 @@ oil.setup({
     ["g\\"] = "actions.toggle_trash",
   },
   use_default_keymaps = false,
+  win_options = {
+    winbar = "%!v:lua.get_oil_winbar()",
+  },
 })
 
 vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
