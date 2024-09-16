9740e48c2f6e0d31ee86473646e039f6c3b90849
Sean Cheng <seanc2841@gmail.com>
2024-09-15 23:07:16 -0400

    Add support for java code

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -129,35 +129,30 @@ local plugins = {
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
-  -- {
-  --   "mfussenegger/nvim-lint", -- linter
-  --   event = {
-  --     "BufReadPre",
-  --     "BufNewFile",
-  --   },
-  --   config = function()
-  --     local lint = require("lint")
-  --
-  --     lint.linters_by_ft = {
-  --       javascript = { "eslint" },
-  --       typescript = { "eslint" },
-  --       javascriptreact = { "eslint" },
-  --       typescriptreact = { "eslint" },
-  --       svelte = { "eslint" },
-  --       python = { "pylint" },
-  --     }
-  --
-  --     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-  --
-  --     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-  --       group = lint_augroup,
-  --       callback = function()
-  --         lint.try_lint()
-  --       end,
-  --     })
-  --
-  --   end,
-  -- },
+  {
+    "mfussenegger/nvim-lint", -- linter
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        java = { "checkstyle" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+  { "mfussenegger/nvim-jdtls" },
   {
     "lervag/vimtex",
     lazy = false,
