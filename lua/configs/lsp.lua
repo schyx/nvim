f6adcc51b6131ba41ef0698495cc7a3f5379ea17
Sean Cheng <secheng@mac.iscinternal.com>
2025-10-16 12:08:05 -0400

    upgrade to 0.10

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,59 +1,30 @@
 require("mason").setup()
 
-local servers = {
-  "lua_ls",
-  "texlab",
-  "ruff",
-  "rust_analyzer",
-  "clangd",
-  "hls",
-  "gopls",
-}
-
 require("mason-lspconfig").setup({
   ensure_installed = {
+    "pyright",
     "lua_ls",
-    "texlab",
-    "ruff",
-    "rust_analyzer",
-    "clangd",
-    "gopls",
   },
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 local on_attach = function() end
 
-local lspconfig = require("lspconfig")
-
-for _, lsp in ipairs(servers) do
-  if lsp ~= "lua_ls" then
-    lspconfig[lsp].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-  end
-end
-
-lspconfig.lua_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
+local lua_settings = {
+  Lua = {
+    runtime = { version = "LuaJIT" },
+    workspace = {
+      library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
       },
-      workspace = {
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
+      maxPreload = 100000,
+      preloadFileSize = 10000,
     },
   },
-})
+}
 
+vim.lsp.config("*", {capabilities = capabilities, on_init = on_attach})
+vim.lsp.config("lua_ls", {settings = lua_settings})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+
