60c3a978e59701ba43005a06c0fe8038085af142
Sean Cheng <seanc2841@gmail.com>
2025-10-17 08:22:46 -0400

    make lua work for nvim 0.11.4

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -3,28 +3,22 @@ require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
     "pyright",
+    "hls",
     "lua_ls",
   },
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local on_attach = function() end
-
-local lua_settings = {
-  Lua = {
-    runtime = { version = "LuaJIT" },
-    workspace = {
-      library = {
-        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
       },
-      maxPreload = 100000,
-      preloadFileSize = 10000,
     },
   },
-}
+})
 
-vim.lsp.config("*", {capabilities = capabilities, on_init = on_attach})
-vim.lsp.config("lua_ls", {settings = lua_settings})
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("hls")
